7b9444879391dd7ac7cc26810f3102fa4a00611e
hikmanisyariful <syarifulfajar017@gmail.com>
2021-06-22 17:12:55 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="header-title">
+        <h1>A Booking Tracking App</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
